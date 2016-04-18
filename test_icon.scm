3e028d71d10970489f4804412c46b94f8826daa4
Hamayama <hamay1010@gmail.com>
2016-04-18 17:40:14 +0900

    test_icon.scm chg

--- test_icon.scm
+++ test_icon.scm
@@ -4,7 +4,15 @@
 (use os.windows)
 (use msicon)
 
-(set-window-icon #f (sys-get-console-title) "icon1.ico")
+(define get-data-path
+  (let1 dir (if-let1 path (current-load-path)
+              (string-append (sys-dirname path) "/") "")
+    (lambda (fname) (string-append dir fname))))
+
+(define *icon-file-path* (get-data-path "icon1.ico"))
+(print  *icon-file-path*)
+
+(set-window-icon #f (sys-get-console-title) *icon-file-path*)
 
 (print "HIT ENTER KEY!")
 (flush)
